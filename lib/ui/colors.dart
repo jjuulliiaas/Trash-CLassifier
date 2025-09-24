558cf38a636c3c2e134872cf33f62ebaf07c516d
juliasidlovska <sidlovska.julia2018@gmail.com>
2025-09-24 23:40:32 +0300

    - rebuild bottom nav bar, - start creating app theme

---
+++ lib/ui/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryGreen = Color.fromARGB(255, 0, 137, 123);
+  static const Color primaryGrey = Color.fromARGB(255, 84, 110, 133);
+}
--- lib/widgets/common/bottom_bar.dart
+++ lib/widgets/common/bottom_bar.dart
@@ -2,50 +2,84 @@ import 'package:fluentui_system_icons/fluentui_system_icons.dart';
 import 'package:flutter/material.dart';
 import 'package:trash_classifier/routes.dart';
 
+import '../../ui/colors.dart';
+
 class CBottomBar extends StatelessWidget{
   const CBottomBar({super.key, required this.currentIndex});
 
   final int currentIndex;
 
+  void _onItemTapped(BuildContext context, int index) {
+    switch(index) {
+      case 0:
+        Navigator.pushReplacementNamed(context, AppRoutes.home);
+        break;
+      case 1:
+        Navigator.pushReplacementNamed(context, AppRoutes.scan);
+        break;
+      case 2:
+        Navigator.pushReplacementNamed(context, AppRoutes.account);
+        break;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return BottomNavigationBar(
-      onTap: (index) {
-        switch(index) {
-          case 0:
-            Navigator.pushReplacementNamed(context, AppRoutes.home);
-          case 1:
-            Navigator.pushReplacementNamed(context, AppRoutes.scan);
-          case 2:
-            Navigator.pushReplacementNamed(context, AppRoutes.account);
-        }
-      },
-      currentIndex: currentIndex,
-      selectedItemColor: Colors.teal,
-      unselectedItemColor: Colors.blueGrey,
-      iconSize: 30,
-      elevation: 0,
-      showSelectedLabels: false,
-      showUnselectedLabels: false,
-      items: const [
-        BottomNavigationBarItem(
-          icon: Icon(
-            FluentIcons.home_20_regular,
-           ),
-          label: '',
+    return SizedBox(
+      child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 16.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            _buildItem(
+              context,
+              index: 0,
+              icon: FluentIcons.home_28_regular,
+              activeIcon: FluentIcons.home_28_filled,
+            ),
+            _buildItem(
+                context,
+                index: 1,
+                icon: FluentIcons.scan_camera_28_regular,
+                size: 50
+            ),
+            _buildItem(
+              context,
+              index: 2,
+              icon: FluentIcons.person_28_regular,
+              activeIcon: FluentIcons.person_28_filled,
+            ),
+          ],
         ),
-        BottomNavigationBarItem(
-          icon: Icon(
-              FluentIcons.scan_camera_20_regular,
-            size: 50,
+      ),
+    );
+  }
+
+  Widget _buildItem(
+      BuildContext context, {
+        required int index,
+        required IconData icon,
+        IconData? activeIcon,
+        double size = 35.0
+      }
+      ) {
+
+    final bool isActive = currentIndex == index;
+
+    return GestureDetector(
+      onTap: () => _onItemTapped(context, index),
+      child: SizedBox(
+        width: 70,
+        height: 70,
+        child: Center(
+          child: Icon(
+            isActive && activeIcon != null ? activeIcon : icon,
+            size: size,
+            color: isActive ? AppColors.primaryGreen : AppColors.primaryGrey,
           ),
-          label: '',
         ),
-        BottomNavigationBarItem(
-          icon: Icon(FluentIcons.person_wrench_20_regular),
-          label: '',
-        ),
-      ],
+      ),
     );
   }
 
